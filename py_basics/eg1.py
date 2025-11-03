a9535ab6029b9a79525a8fc972b4d77e9a1fb30b
Tufail <tufailbabacoc@gmail.com>
2025-11-03 22:22:20 +0530

    inpuut & print

---
+++ py_basics/eg1.py
@@ -0,0 +1,5 @@
+firstname = input('First Name: ' )
+lastname = input('Last Name: ' )
+age = input('age: ')
+
+print("Your full name is ", firstname," ",lastname ,' ', "and your are ",age,'years old')

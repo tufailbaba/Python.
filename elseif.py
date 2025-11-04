d99418e9e66a13afa878b52f6cd266bf1d5432c4
Tufail <tufailbabacoc@gmail.com>
2025-11-04 08:15:50 +0530

    ifelse

---
+++ elseif.py
@@ -0,0 +1,24 @@
+# name = 'Tufail'
+
+# if name == 'Tufail':
+#     print("You are welcome")
+# else:
+#     print('You can leave')
+    
+    
+name1 = input('Name: ')
+if name1 == 'Tufail baba':
+    print('Hey Tufail baba')
+elif name1 == 'Tufail':
+    print('Hey tufi')
+else:
+    print('You are not Tufail')
+    
+    
+    
+    
+
+
+
+
+    

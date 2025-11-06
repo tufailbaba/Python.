657a65b2abce96ad572655a5585c17c219601c80
Tufail <tufailbabacoc@gmail.com>
2025-11-06 11:41:56 +0530

    Practice time

--- typecast.py
+++ typecast.py
@@ -5,9 +5,26 @@ num2 = float(num)
 print(type(num2))
 print(num2)
 
-name = 'Tufail'
-print(name)
-print(type(name))
-name2 = float(name)
-print(type(name2))
-print(name2)
+# name = 'Tufail'
+# print(name)
+# print(type(name))
+# name2 = float(name)
+# print(type(name2))
+# print(name2)
+
+num = input("Enter a decimal number: ")
+print(type(num))
+num2 = float(num)
+print(type(num2))
+print(num2)
+num3 = int(num2)
+print(type(num3))
+print(num3)
+
+
+#practice time
+num4 = input("Enter an integer number: ")
+print(type(num4))
+num5 = int(num4)
+print(type(num5))
+print(num5)

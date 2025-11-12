42897e828889885d901f0e5f92b20a0d6a2b8c9b
Tufail <tufailbabacoc@gmail.com>
2025-11-12 14:50:39 +0530

    whileloops

---
+++ whileloop.py
@@ -0,0 +1,12 @@
+# num = 0
+# while(num <=25):
+#     num += 5
+    
+#     print(num)
+    
+num = float(input("Enter a number "))    
+num2 = float(input("Enter a range "))
+
+while(num <= num2):
+    num += 5
+    print(num)    

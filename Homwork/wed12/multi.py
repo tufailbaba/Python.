5c987f6869a36aac4340c5fcb71cc4e8e9a7fe71
Tufail <tufailbabacoc@gmail.com>
2025-11-14 13:41:03 +0530

    swapping 2 numbers wo 3 variable

---
+++ Homwork/wed12/multi.py
@@ -0,0 +1,6 @@
+#wap to print multiplication table of a given number using for loop
+num = int(input("Enter a number"))
+
+for i in range (1,11): #Here the entered number will get multiplied 10 times
+    print(i * num)
+          
---
+++ Homwork/wed12/swap.py
@@ -0,0 +1,6 @@
+#Swapping values
+num1 = input("Enter 1st number ")
+num2 = input("Enter 2nd number ")
+print("Before swapping 1st number is: ",num1,"and 2nd number is: ",num2)
+num1, num2 = num2, num1
+print("After swapping 1st number is: ",num2, "and 2nd number is: ",num1)
